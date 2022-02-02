66686fa5c76b3b3901a05a85cb7b0b1a44029513
007tickooayush <ayush.tickoo292000@gmail.com>
2022-02-02 19:59:20 +0530

    game history handling added

--- src/App.jsx
+++ src/App.jsx
@@ -5,40 +5,53 @@ import { calculateWinner } from './helpers';
 import './styles/root.scss';
 
 const App = () => {
-  const [board, setBoard] = useState(Array(9).fill(null));
-  const [isXNext, setIsXNext] = useState(false);
-  const winner = calculateWinner(board);
+  const [history, setHistory] = useState([
+    { board: Array(9).fill(null), isXNext: true },
+  ]);
 
+  // const [isXNext, setIsXNext] = useState(false);
+
+  const [currentMove, setCurrentMove] = useState(0);
+  const current = history[currentMove];
+
+  const winner = calculateWinner(current.board);
   const message = winner
     ? `Winner is ${winner}`
-    : `Next Player is  ${isXNext ? 'X' : 'O'}`;
-  // console.log(board);
+    : `Next Player is  ${current.isXNext ? 'X' : 'O'}`;
+
+  
+  console.log(history);
   // console.log(winner);
 
   const handleSquareClick = position => {
-    // prevent used position updation 
-    if (board[position] || winner) {
+    // prevent used position updation
+    if (current.board[position] || winner) {
       return;
     }
 
-    setBoard(prev => {
-      return prev.map((square, pos) => {
-        if (pos === position) {
-          return isXNext ? 'X' : 'O';
-        }
+    setHistory(prev => {
+      
+      const last = prev[prev.length-1];
 
+      const newBoard = last.board.map((square, pos) => {
+        if (pos === position) {
+          return last.isXNext ? 'X' : 'O';
+        }
+      
         return square;
       });
+
+      return prev.concat({board:newBoard,isXNext: !last.isXNext});
     });
 
-    setIsXNext(prev => !prev);
+    setCurrentMove( prev => prev+1)
   };
 
   return (
     <div className="app">
       <h1>TIC TAC TOE</h1>
       <h2>{message}</h2>
-      <Board board={board} handleSquareClick={handleSquareClick} />
+      <Board board={current.board} handleSquareClick={handleSquareClick} />
     </div>
   );
 };
